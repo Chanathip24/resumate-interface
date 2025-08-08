6da056c9bab2c9fbc8e3045befb97ed17a269fff
Chanathip <chanathip.cha@outlook.com>
2025-08-08 10:12:48 +0700

    feat/init

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,12 @@
 const HomePage = () => {
   return (
+    // <div className="container mx-auto flex min-h-[calc(100dvh-var(--header-height))] flex-col items-center gap-y-4 py-4">
+    //   <div className="grid-background" />
+    //   <h1>ResuMATE</h1>
+    // </div>
     <div className="container mx-auto flex min-h-[calc(100dvh-var(--header-height))] flex-col items-center gap-y-4 py-4">
-      <h1>ResuMATE</h1>
+      <div className="grid-background" />
+      <h1 className="gradient-title">TEST</h1>
     </div>
   )
 }
