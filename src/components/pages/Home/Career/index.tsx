5e421ab96de42da77a7ba53c98002a785507bd29
Varich Maleevan <varich.malee@gmail.com>
2025-08-09 23:22:44 +0700

    refactor: simplify Career component structure by removing unnecessary div wrapper

--- src/components/pages/Home/Career/index.tsx
+++ src/components/pages/Home/Career/index.tsx
@@ -3,17 +3,15 @@ import { Section } from '@/components/pages/Home/Section'
 
 const Career = () => {
   return (
-    <Section>
-      <div className="flex w-4/5 flex-col justify-start gap-y-3">
-        <h2>Start Your Real Career Today</h2>
-        <p className="text-theme-gray-600">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, dolorem autem nesciunt, iste repellendus
-          soluta ex labore consectetur reprehenderit delectus nostrum deleniti voluptates aspernatur corrupti beatae
-          accusamus praesentium aliquam magnam! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis,
-          voluptate ipsa ut blanditiis sed inventore perspiciatis dolores, et dolorum, cumque veniam sequi. Animi, amet
-          dolorum. Repellendus rem illo facere iusto.
-        </p>
-      </div>
+    <Section contentClassName="flex w-4/5 flex-col justify-start gap-y-3">
+      <h2>Start Your Real Career Today</h2>
+      <p className="text-theme-gray-600">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, dolorem autem nesciunt, iste repellendus
+        soluta ex labore consectetur reprehenderit delectus nostrum deleniti voluptates aspernatur corrupti beatae
+        accusamus praesentium aliquam magnam! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis,
+        voluptate ipsa ut blanditiis sed inventore perspiciatis dolores, et dolorum, cumque veniam sequi. Animi, amet
+        dolorum. Repellendus rem illo facere iusto.
+      </p>
       <Button variant="primary">Explore Properties</Button>
     </Section>
   )
