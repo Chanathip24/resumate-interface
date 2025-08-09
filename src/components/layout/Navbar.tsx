da15f788c137fc2d0b9ac906f35c5bb625523f22
Chanathip <chanathip.cha@outlook.com>
2025-08-09 09:51:27 +0700

    feat : Add Navbar with version , sign in button

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,26 +1,32 @@
-import { LogOutIcon, UserIcon } from 'lucide-react'
+import { Package } from 'lucide-react'
 import { Link } from 'react-router-dom'
 
-import { Button, DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/components/common'
+import { Button } from '@/components/common'
 import { ROUTES, VERSION } from '@/constants'
 
 const NavBar = () => {
   // hooks
 
   return (
-    <div className="bg-theme-night border-theme-night sticky top-0 z-20 box-border flex h-16 w-full flex-row items-center justify-between gap-x-10 border-b px-8">
-      <Link to={ROUTES.home.path}>
-        <h4 className="text-theme-white">ResuMATE</h4>
-      </Link>
-      <div className="flex w-fit flex-row items-center justify-center gap-x-2">
-        <span className="text-theme-white">{VERSION}</span>
-        <Button
-          asChild
-          className="text-primary flex flex-row items-center justify-center gap-x-1 px-3"
-          size="sm"
-          variant="primary"
-        />
-        <DropdownMenu>
+    <div className="sticky top-0 z-20 flex w-full items-center justify-center py-2">
+      <div className="bg-theme-night-300 border-theme-night box-border flex h-16 w-4/5 flex-row items-center justify-between gap-x-10 rounded-lg border-b px-8">
+        <Link to={ROUTES.home.path} className="flex items-center gap-x-2">
+          <Package className="bg-theme-white text-theme-night rounded-lg p-1" size={30} />
+          <h4 className="text-theme-white">ResuMATE</h4>
+        </Link>
+        {/* menu */}
+        <div className="flex w-fit flex-row items-center justify-center gap-x-2">
+          <span className="text-theme-white">{VERSION}</span>
+          <Button
+            asChild
+            className="text-primary flex flex-row items-center justify-center gap-x-1 px-3"
+            size="sm"
+            variant="primary"
+          />
+          <Button size="sm" variant="outline">
+            Sign In
+          </Button>
+          {/* <DropdownMenu>
           <DropdownMenuTrigger asChild>
             <Button
               className="hover:[&>*]:text-primary data-[state=open]:bg-theme-primary data-[state=open]:[&>*]:text-primary flex aspect-square items-center justify-center p-0"
@@ -36,7 +42,8 @@ const NavBar = () => {
               <h5>Logout</h5>
             </DropdownMenuItem>
           </DropdownMenuContent>
-        </DropdownMenu>
+        </DropdownMenu> */}
+        </div>
       </div>
     </div>
   )
