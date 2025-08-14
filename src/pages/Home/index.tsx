b123b2f2fa2652b7fe0c85db47c771022b582dd2
Chanathip <chanathip.cha@outlook.com>
2025-08-14 20:37:00 +0700

    feat/our-service-section

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,13 @@
-import { Career, Hero, Question } from '@/components/pages/Home'
+import { Career, Hero, Question, Service } from '@/components/pages/Home'
 
 const HomePage = () => {
   return (
     <div className="flex size-full flex-col items-center justify-center">
       <Hero />
+      <Service />
       <Question />
       <Career />
+      <Service />
     </div>
   )
 }
